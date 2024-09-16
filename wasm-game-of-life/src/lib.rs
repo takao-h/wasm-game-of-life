cff0e695e21ddbd1743d8430de17ce189f2d591e
takao-h <takao.hayashi@zozo.com>
2024-09-17 00:12:32 +0900

    I was able to display helloworld.

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -1,20 +1,6 @@
-extern crate cfg_if;
-extern crate wasm_bindgen;
-
 mod utils;
 
-use cfg_if::cfg_if;
 use wasm_bindgen::prelude::*;
-
-cfg_if! {
-    // `wee_alloc` featureが有効になっているとき、
-    // `wee_alloc`をグローバルアロケータとして使います。
-    if #[cfg(feature = "wee_alloc")] {
-        extern crate wee_alloc;
-        #[global_allocator]
-        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-    }
-}
 
 #[wasm_bindgen]
 extern "C" {
